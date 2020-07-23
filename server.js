044d12e339eb83fab0e17c5e4b450bec80e56903
Shane4666 <sc897@nau.edu>
2020-07-23 12:11:08 -0700

    initial server setup.

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+// API Routes
+app.get('/', (req, res) => res.send('Apex Api.'));
+app.get('/players', require('routes/api/players'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
