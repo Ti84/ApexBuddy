c66052ed92da0d3cc2ff220726cf4bec9c4508dc
Shane4666 <sc897@nau.edu>
2020-07-30 15:18:18 -0700

    legend based backgrounds.

--- client/src/components/layout/Theme.js
+++ client/src/components/layout/Theme.js
@@ -2,28 +2,32 @@ import React from 'react';
 import { makeStyles, ThemeProvider, createMuiTheme } from '@material-ui/core';
 import useLegend, { ApexLegendContext } from './useLegend';
 
-const useThemeStyles = makeStyles((theme) => ({
-  theme: {
-    height: '100vh',
-    background: props => `url(${props.imgUrl}) no-repeat 50% fixed`,
-    boxShadow: `0px 0px 150px #274669 inset`,
-    position: 'relative',
-    zIndex: 0,
-  },
-  themeContainer: {
-    '&::after': {
-      backgroundColor: '#274669',
-      content: `''`,
-      height: '100vh',
-      width: '100vw',
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      opacity: 0.4,
-      zIndex: -1,
+const useThemeStyles = makeStyles((theme) => {
+  console.log(theme);
+  return {
+    theme: {
+      backgroundImage: (props) => `url(${props.imgUrl})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: props => `center top ${props.backgroundTopOffset}`,
+      boxShadow: `0px 0px 150px #274669 inset`,
+      position: 'relative',
+      zIndex: 0,
     },
-  },
-}));
+    themeContainer: {
+      '&::after': {
+        backgroundColor: '#274669',
+        content: `''`,
+        height: '100vh',
+        width: '100vw',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        opacity: 0.4,
+        zIndex: -1,
+      },
+    },
+  };
+});
 
 const Theme = ({ children }) => {
   const [apexLegendData, setApexLegend] = useLegend('wraith');
--- client/src/components/layout/useLegend.js
+++ client/src/components/layout/useLegend.js
@@ -8,10 +8,103 @@ const useLegend = (name) => {
 
   useEffect(() => {
     switch (apexLegend) {
-      case 'wraith':
+      case 'bangalore':
         setApexLegendData({
-          name: 'wraith',
-          imgUrl: 'https://wallpapercave.com/wp/wp4573120.png',
+          name: 'bangalore',
+          imgUrl: 'https://i.redd.it/xwlfy8208dh21.png',
+          backgroundTopOffset: '0',
+          theme: {},
+        });
+        break;
+      case 'bloodhound':
+        setApexLegendData({
+          name: 'bloodhound',
+          imgUrl: 'https://images.alphacoders.com/989/thumb-1920-989913.png',
+          backgroundTopOffset: '50px',
+          theme: {},
+        });
+        break;
+      case 'caustic':
+        setApexLegendData({
+          name: 'caustic',
+          imgUrl: 'https://wallpapercave.com/wp/wp4578108.jpg',
+          backgroundTopOffset: '50px',
+          theme: {},
+        });
+        break;
+      case 'crypto':
+        setApexLegendData({
+          name: 'crypto',
+          imgUrl:
+            'https://image5.uhdpaper.com/wallpaper-hd/apex-legends-crypto-uhdpaper.com-hd-5.1101.jpg',
+            backgroundTopOffset: '50px',
+          theme: {},
+        });
+        break;
+      case 'gibraltar':
+        setApexLegendData({
+          name: 'gibraltar',
+          imgUrl:
+            'https://i.pinimg.com/originals/df/bd/6c/dfbd6cade407bcf9054220c77b667749.jpg',
+            backgroundTopOffset: '50px',
+          theme: {},
+        });
+        break;
+      case 'lifeline':
+        setApexLegendData({
+          name: 'lifeline',
+          imgUrl: 'https://wallpapercave.com/wp/wp5035870.png',
+          backgroundTopOffset: '0',
+          theme: {},
+        });
+        break;
+      case 'loba':
+        setApexLegendData({
+          name: 'loba',
+          imgUrl:
+            'https://image3.uhdpaper.com/wallpaper-hd/loba-apex-legends-art-uhdpaper.com-hd-3.1893.jpg',
+            backgroundTopOffset: '50px',
+          theme: {},
+        });
+        break;
+      case 'mirage':
+        setApexLegendData({
+          name: 'mirage',
+          imgUrl:
+            'https://cdna.artstation.com/p/assets/images/images/016/214/252/large/ellyan-theodoro-mirage.jpg?1551336097',
+            backgroundTopOffset: '50px',
+          theme: {},
+        });
+        break;
+      case 'octane':
+        setApexLegendData({
+          name: 'octane',
+          imgUrl: 'https://wallpapercave.com/wp/wp4636586.jpg',
+          backgroundTopOffset: '50px',
+          theme: {},
+        });
+        break;
+      case 'pathfinder':
+        setApexLegendData({
+          name: 'pathfinder',
+          imgUrl: 'https://wallpapers.gry-online.pl/pliki/70763/1920x1080.jpg',
+          backgroundTopOffset: '50px',
+          theme: {},
+        });
+        break;
+      case 'revenant':
+        setApexLegendData({
+          name: 'revenant',
+          imgUrl: 'https://wallpapercave.com/wp/wp5505586.jpg',
+          backgroundTopOffset: '50px',
+          theme: {},
+        });
+        break;
+      case 'wattson':
+        setApexLegendData({
+          name: 'wattson',
+          imgUrl: 'https://wallpapercave.com/wp/wp4720540.jpg',
+          backgroundTopOffset: '50px',
           theme: {},
         });
         break;
@@ -19,6 +112,7 @@ const useLegend = (name) => {
         setApexLegendData({
           name: 'wraith',
           imgUrl: 'https://wallpapercave.com/wp/wp4573120.png',
+          backgroundTopOffset: '0',
           theme: {},
         });
     }
